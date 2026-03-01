26ab1a6be05824de94cc676216d27a5b2799e6d6
yofsh <to@yof.sh>
2026-03-01 03:12:35 +0100

    added hdr support parts

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -3,7 +3,13 @@
   environment.systemPackages = with pkgs; [
     mangohud
     protonup-qt
+    vulkan-hdr-layer-kwin6
   ];
+
+  environment.sessionVariables = {
+    DXVK_HDR = "1";
+    ENABLE_HDR_WSI = "1";
+  };
 
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
@@ -13,5 +19,5 @@
 
   programs.gamemode.enable = true;
   programs.gamescope.enable = true;
-  programs.gamescope.capSysNice = true;
+  programs.gamescope.capSysNice = false;
 }
